4d9e39cd1a0fbd4817737a3c995014e3b8ae757f
Al Newkirk <anewkirk@ana.io>
2015-09-28 13:10:04 +0000

    Updated inconsistency in documentation.

--- lib/API/Trello.pm
+++ lib/API/Trello.pm
@@ -167,7 +167,7 @@ The debug attribute if true prints HTTP requests and responses to standard out.
     $trello->fatal(1);
 
 The fatal attribute if true promotes 4xx and 5xx server response codes to
-exceptions, a L<API::Trello::Exception> object.
+exceptions, a L<API::Client::Exception> object.
 
 =cut
 
